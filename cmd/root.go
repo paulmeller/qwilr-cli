2c786f7ff2226971e6eff333e1b5e40351e24b9e
Paul Meller <14568+paulmeller@users.noreply.github.com>
2026-04-24 06:54:41 +1000

    feat: scaffold project with Cobra root command

---
+++ cmd/root.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var jsonOutput bool
+
+var rootCmd = &cobra.Command{
+	Use:   "qwilr",
+	Short: "CLI for the Qwilr API",
+	Long:  "Manage Qwilr pages, templates, blocks, and webhooks from the command line.",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().BoolVar(&jsonOutput, "json", false, "Output in JSON format")
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("$HOME/.config/qwilr")
+	viper.SetEnvPrefix("QWILR")
+	viper.AutomaticEnv()
+	_ = viper.ReadInConfig()
+}
