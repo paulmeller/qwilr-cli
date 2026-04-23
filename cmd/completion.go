d307157dfbcc0f6db7fa71693329675dfddb71c6
Paul Meller <14568+paulmeller@users.noreply.github.com>
2026-04-24 07:01:44 +1000

    feat: add shell completion command (bash/zsh/fish)

---
+++ cmd/completion.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish]",
+	Short: "Generate shell completion scripts",
+	Long: `Generate shell completion scripts for bash, zsh, or fish.
+
+To load completions:
+
+Bash:
+  $ source <(qwilr completion bash)
+
+Zsh:
+  $ qwilr completion zsh > "${fpath[1]}/_qwilr"
+
+Fish:
+  $ qwilr completion fish | source`,
+	Args:      cobra.ExactValidArgs(1),
+	ValidArgs: []string{"bash", "zsh", "fish"},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		switch args[0] {
+		case "bash":
+			return rootCmd.GenBashCompletion(os.Stdout)
+		case "zsh":
+			return rootCmd.GenZshCompletion(os.Stdout)
+		case "fish":
+			return rootCmd.GenFishCompletion(os.Stdout, true)
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
